1973daf88752f003273cb7c3e71480396421b779
Fatih <budakf@itu.edu.tr>
2020-03-23 18:45:00 +0300

    created custom input dialog for recorder operation

--- multipleinputdialog.cpp
+++ multipleinputdialog.cpp
@@ -1,6 +1,38 @@
 #include "multipleinputdialog.h"
 
-MultipleInputDialog::MultipleInputDialog()
-{
+MultipleInputDialog::MultipleInputDialog(QStringList pLabels, QWidget *parent) : QDialog(parent){
+    setLayout(pLabels);
+}
 
+
+void MultipleInputDialog::setLayout(QStringList pLabels){
+    mMainLayout = new QFormLayout(this);
+
+    foreach(QString label, pLabels) {
+        QLineEdit* lineEdit = new QLineEdit(this);
+        mLineEditFields.push_back(lineEdit);
+        mMainLayout->addRow(label, lineEdit);
+    }
+    mButtonBox = new QDialogButtonBox(QDialogButtonBox::Ok |
+                     QDialogButtonBox::Cancel, Qt::Horizontal, this);
+    mMainLayout->addRow(mButtonBox);
+
+    connect(mButtonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(mButtonBox, SIGNAL(rejected()), this, SLOT(reject()));
+}
+
+
+QStringList MultipleInputDialog::getTexts(QStringList pLabels, QWidget *parent, QLineEdit::EchoMode echo,
+                                          Qt::WindowFlags flags,Qt::InputMethodHints inputMethodHints){
+
+    MultipleInputDialog* multipleDialog = new MultipleInputDialog(pLabels,parent);
+    QStringList lines;
+
+    if (multipleDialog->exec() == QDialog::Accepted){
+        foreach(QLineEdit* lineEdit, multipleDialog->mLineEditFields){
+            lines << lineEdit->text();
+        }
+    }
+    qDebug()<<lines;
+    return  lines;
 }
--- multipleinputdialog.h
+++ multipleinputdialog.h
@@ -1,11 +1,39 @@
 #ifndef MULTIPLEINPUTDIALOG_H
 #define MULTIPLEINPUTDIALOG_H
 
+#include <QTimer>
+#include <QDebug>
+#include <QDialog>
+#include <QWidget>
+#include <QLineEdit>
+#include <QFormLayout>
+#include <QDialogButtonBox>
 
-class MultipleInputDialog : public QInputDialog
-{
+#include <QString>
+#include <QStringList>
+
+class MultipleInputDialog : public QDialog{
+    Q_OBJECT
+
 public:
-    MultipleInputDialog();
+    MultipleInputDialog(QStringList pFieldNames, QWidget* parent=nullptr);
+    static QStringList getTexts(QStringList, QWidget* parent=nullptr, QLineEdit::EchoMode echo = QLineEdit::Normal,
+                                Qt::WindowFlags flags = Qt::WindowFlags(),
+                                Qt::InputMethodHints inputMethodHints = Qt::ImhNone);
+private:
+    QList<QLineEdit*> mLineEditFields;
+    QFormLayout* mMainLayout;
+    QDialogButtonBox* mButtonBox;
+
+    void setLayout(QStringList pFieldNames);
+
+public slots:
+//    void accept() override;
+//    void reject() override;
+
+signals:
+
+
 };
 
 #endif // MULTIPLEINPUTDIALOG_H
